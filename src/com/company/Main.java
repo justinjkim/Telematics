b3f8920d90ee5a03f5adb3950219e72ddc01ddb4
Justin Kim <jjkim928@gmail.com>
2017-09-16 17:47:33 -0400

    ask user for input for all variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,24 @@ public class Main {
 
         VehicleInfo newCar = new VehicleInfo();
 
+        // obtain input from the user and set values
         System.out.println("Please enter your vehicle's VIN:");
         newCar.setVIN(userInput.nextInt());
 
-        System.out.println(newCar.getVIN());
+        System.out.println("Please enter your vehicle's current odometer:");
+        newCar.setOdometer(userInput.nextDouble());
+
+        System.out.println("Please enter how many gallons of gas your vehicle has consumed:");
+        newCar.setConsumption(userInput.nextDouble());
+
+        System.out.println("Please enter the odometer reading of your last oil change:");
+        newCar.setOdometerSinceLastOilChange(userInput.nextDouble());
+
+        System.out.println("Please enter your vehicle's engine size in liters:");
+        newCar.setEngineSizeInLiters(userInput.nextDouble());
+
+
+        
 
 
 
