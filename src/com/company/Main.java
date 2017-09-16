5a974b5bdda5f76131d5f6d60f5a7b53f6caa609
Justin Kim <jjkim928@gmail.com>
2017-09-16 17:39:03 -0400

    test - get user input to set the VIN for a new VehicleInfo instance

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner userInput = new Scanner(System.in);
+
+        VehicleInfo newCar = new VehicleInfo();
+
+        System.out.println("Please enter your vehicle's VIN:");
+        newCar.setVIN(userInput.nextInt());
+
+        System.out.println(newCar.getVIN());
+
+
+
+
+
     }
 }
