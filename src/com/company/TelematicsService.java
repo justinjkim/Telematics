e35a18d24442feb1d315807efe78311d6570d7ea
Justin Kim <jjkim928@gmail.com>
2017-09-17 09:55:02 -0400

    added functionality to write user input into JSON

--- src/com/company/TelematicsService.java
+++ src/com/company/TelematicsService.java
@@ -1,7 +1,18 @@
 package com.company;
 
-public class TelematicsService {
-    void report(VehicleInfo vehicleInfo) {
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.IOException;
+
+public class TelematicsService {
+
+    static void report(VehicleInfo vehicleInfo) throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        int VIN = vehicleInfo.getVIN();
+        mapper.writeValue(new File(VIN + ".json"), vehicleInfo); // this line, I had to look up online
+        String json = mapper.writeValueAsString(vehicleInfo);
+        System.out.println(json);
     };
 }
