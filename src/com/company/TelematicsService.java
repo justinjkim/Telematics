01a1c2de85fe818c22a282ba756e946f3caf6bb2
Justin Kim <jjkim928@gmail.com>
2017-09-18 17:30:34 -0400

    added functionality to convert JSON to Java and output to console

--- src/com/company/TelematicsService.java
+++ src/com/company/TelematicsService.java
@@ -7,12 +7,41 @@ import java.io.IOException;
 
 public class TelematicsService {
 
-    static void report(VehicleInfo vehicleInfo) throws IOException{ // need to understand why throwing IO exception here works
+    public static void report(VehicleInfo vehicleInfo) throws IOException{ // need to understand why throwing IO exception here works
 
         ObjectMapper mapper = new ObjectMapper();
+
+        // get the VIN from Vehicle Info
         int VIN = vehicleInfo.getVIN();
+
+        // write new File based on VIN
         mapper.writeValue(new File(VIN + ".json"), vehicleInfo); // this line, I had to look up online
+
+        // test to make sure json is formatted properly
         String json = mapper.writeValueAsString(vehicleInfo);
         System.out.println(json);
     };
+
+    public static void convertToJava() throws IOException {
+        File file = new File(".");
+        for (File f : file.listFiles()) {
+            if (f.getName().endsWith(".json")) {
+                // Now you have a File object named "f".
+                // You can use this to create a new instance of Scanner
+                ObjectMapper mapper = new ObjectMapper();
+                VehicleInfo vi = mapper.readValue(f, VehicleInfo.class);
+                System.out.println(vi);
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+
+
 }
