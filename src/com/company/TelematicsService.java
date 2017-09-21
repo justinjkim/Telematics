f802921ab98f1046829c14f215a5d400ecb69ee7
Justin Kim <jjkim928@gmail.com>
2017-09-20 22:41:49 -0400

    added lots of HTML

--- src/com/company/TelematicsService.java
+++ src/com/company/TelematicsService.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TelematicsService {
 
@@ -23,6 +25,7 @@ public class TelematicsService {
     };
 
     public static void convertToJava() throws IOException {
+        List<VehicleInfo> vehicles = new ArrayList<>();
         File file = new File(".");
         for (File f : file.listFiles()) {
             if (f.getName().endsWith(".json")) {
@@ -30,15 +33,56 @@ public class TelematicsService {
                 // You can use this to create a new instance of Scanner
                 ObjectMapper mapper = new ObjectMapper();
                 VehicleInfo vi = mapper.readValue(f, VehicleInfo.class);
-                System.out.println(vi);
+                //System.out.println(vi);
+                vehicles.add(vi);
             }
         }
+        System.out.println(vehicles);
     }
 
-    public static void updateDashboard() {
-        double avgOdometer;
-        double avgConsumption;
-        double avgLastOilChange;
+    public static void updateDashboard(List<VehicleInfo> vehicles) {
+        double avgOdometer = 0;
+        double avgConsumption = 0;
+        double avgLastOilChange = 0;
+        String updatedHTML = "";
+
+        File file = new File("dashboard.html");
+
+        for (VehicleInfo vehicle: vehicles) {
+            avgOdometer += vehicle.getOdometer();
+            avgConsumption += vehicle.getConsumption();
+            avgLastOilChange += vehicle.getOdometerSinceLastOilChange();
+        }
+
+        updatedHTML +=
+                "<html>" + "<title>Vehicle Telematics Dashboard</title> " +
+                "<body> " +
+                "<h1 align=\"center\">Averages for # vehicles</h1> " +
+                "<table align=\"center\"> " +
+                "<tr> " +
+                "<th>Odometer (miles) |</th><th>Consumption (gallons) |</th><th>Last Oil Change |</th><th>Engine Size (liters)</th>" +
+                "</tr> " +
+                "<tr> " +
+                "<td align=\"center\">#</td><td align=\"center\">#</td><td align=\"center\">#</td align=\"center\"><td align=\"center\">#</td> " +
+                "</tr> " +
+                "</table> " +
+                "<h1 align=\"center\">History</h1> " +
+                "<table align=\"center\" border=\"1\"> " +
+                "<tr> " +
+                "<th>VIN</th><th>Odometer (miles)</th><th>Consumption (gallons)</th><th>Last Oil Change</th><th>Engine Size (liters)</th> " +
+                "</tr> " +
+                "<tr> " +
+                "<td align=\"center\">#</td><td align=\"center\">#</td><td align=\"center\">#</td><td align=\"center\">#</td align=\"center\"><td align=\"center\">#</td> " +
+                "</tr> " +
+                "<tr> " +
+                "<td align=\"center\">45435</td><td align=\"center\">123</td><td align=\"center\">234</td><td align=\"center\">345</td align=\"center\"><td align=\"center\">4.5</td> " +
+                "</tr> " +
+                "</table> " +
+                "</body> " +
+                "</html>";
+
+
+
     }
 
 
